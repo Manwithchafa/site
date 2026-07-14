a3427a436543a611db3299aa21d4bb9100fcc39f
DjCeejay <lordceejay12@gmail.com>
2026-07-14 13:28:47 +0100

    Add church logo favicon

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ $title ?? 'Admin' }} · {{ $currentChurch->name ?? config('app.name', 'Church ERP') }}</title>
 
+        <link rel="icon" type="image/png" href="{{ asset('images/CHlogo.png') }}">
+        <link rel="apple-touch-icon" href="{{ asset('images/CHlogo.png') }}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
